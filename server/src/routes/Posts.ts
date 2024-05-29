dc449dd65d7661ff190ca632cbcc15086e097a61
Elvir Misini <elvirmisini@gmail.com>
2024-05-29 15:57:08 +0200

    create Post

--- server/src/routes/Posts.ts
+++ server/src/routes/Posts.ts
@@ -1,9 +1,20 @@
 import { Request, Response, Router } from 'express';
-
+const {Posts}= require('../../models')
 const router = Router();
 
 router.get('/',(req, res) => {
     res.send('Hello World!');
+  });
+  
+  router.post('/',async (req, res) => {
+    const posts=req.body
+    await Posts.create(posts)
+    res.send(posts)
+  });
+
+  router.get('/all-posts',async (req, res) => {
+    const posts=await Posts.findAll()
+    res.send(posts)
   });
 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 const router = Router()
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
