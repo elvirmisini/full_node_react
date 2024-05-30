7357aaa43981881360a75830d50b23bbc548a67d
Elvir Misini <elvirmisini@gmail.com>
2024-05-30 09:46:03 +0200

    started react app, fetch all posts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,12 @@
 import express, { Router } from 'express';
 import PostRouter from './routes/Posts'
 const db = require("../models");
-
+import cors from "cors";
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const router = Router()
+app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!');
