eabdc366ab4fbb48e353190a201cf4902012af51
Elvir Misini <elvirmisini@gmail.com>
2024-06-02 00:14:16 +0200

    refresh page after comment creation

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -7,6 +7,9 @@ function Post() {
     let {id}=useParams()
     const [postObject,setPostObject]=useState({})
     const [comments,setComments]=useState([])
+    const [newComment,setNewComment]=useState("")
+
+
     useEffect(()=>{
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response)=>{
             setPostObject(response.data)
@@ -14,8 +17,15 @@ function Post() {
     useEffect(()=>{
         axios.get(`http://localhost:3001/comments/${id}`).then((response)=>{
             setComments(response.data)
-    })}
+    })},[]
     )
+    const addComment=()=>{
+      axios.post(`http://localhost:3001/comments`,{commentBody:newComment,PostId:id}).then((response)=>{
+      const commentToAdd={commentBody:newComment}      
+      setComments([...comments,commentToAdd])
+      setNewComment("")
+    })}
+    
   return (
     <div className="postPage">
       <div className="leftSide">
@@ -27,8 +37,8 @@ function Post() {
       </div>
       <div className="rightSide">
         <div className='addCommentContainer'>
-          <input type='text' placeholder='Comment...' autoComplete='off'></input>
-          <button>Add Comment</button>
+          <input type='text' placeholder='Comment...' autoComplete='off' value={newComment} onChange={(event)=>{setNewComment(event.target.value)}}></input>
+          <button onClick={addComment}>Add Comment</button>
           <div className='listOfComments'>
             {comments.map((comment,key)=>{
               return <div key={key} className='comment'>{comment.commentBody}</div>
