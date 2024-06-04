8028cbce17e9771144513b3cc7101c5b8a97534c
Elvir Misini <elvirmisini@gmail.com>
2024-06-04 13:41:21 +0200

    Revert "small change"
    This reverts commit 91a68c1da00157d788419aa3c03c1c643931eece.

--- server/src/routes/Posts.ts
+++ server/src/routes/Posts.ts
@@ -1,20 +1,27 @@
 import { Request, Response, Router } from 'express';
-
+const {Posts}= require('../../models')
 const router = Router();
-const{Posts}=require('../../models')
-
 
 router.get('/',(req, res) => {
     res.send('Hello World!');
   });
+  
+  router.post('/',async (req, res) => {
+    const posts=req.body
+    await Posts.create(posts)
+    res.send(posts)
+  });
 
+  router.get('/all-posts',async (req, res) => {
+    const posts=await Posts.findAll()
+    res.send(posts)
+  });
 
-router.post('/',async (req, res) => {
-    const post=req.body
-    await Posts.create(post)
-
+  router.get('/byId/:id',async (req, res) => {
+    const id=req.params.id
+    const post=await Posts.findByPk(id)
+    console.log(post)
     res.json(post)
-    
   });
 
 
